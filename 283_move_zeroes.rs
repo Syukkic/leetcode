488c6d1eab992eb8c317d396fbe15949ad861b87
Yuki.N <Syukkic@gmail.com>
2024-12-16 16:12:53 +0800

    Solved

---
+++ 283_move_zeroes.rs
@@ -0,0 +1,17 @@
+// https://leetcode.com/problems/move-zeroes
+
+impl Solution {
+    pub fn move_zeroes(nums: &mut Vec<i32>) {
+        let mut non_zero_index = 0;
+        for i in 0..nums.len() {
+            if nums[i] != 0 {
+                nums[non_zero_index] = nums[i];
+                non_zero_index += 1;
+            }
+        }
+
+        for i in non_zero_index..nums.len() {
+            nums[i] = 0;
+        }
+    }
+}
